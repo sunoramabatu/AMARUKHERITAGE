5d916bdfcab48d03e47a4b1b154c46c29f7a63b3
sunoramabatu@gmail.com <emailanda@gmail.com>
2026-03-16 21:54:32 +0700

    capek ganti page.jstambah/

--- app/tambah/page.js
+++ app/tambah/page.js
@@ -1,22 +1,11 @@
-"use client"
+"use client";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
-import { useSearchParams } from "next/navigation"
 import { Suspense } from "react";
 import TambahAnggotaClient from "./TambahAnggotaClient";
 
-
 export default function Page() {
-
-  const params = useSearchParams()
-
-  const pasanganId = params.get("pasangan")
-  const orangtuaId = params.get("orangtua")
-
-  const modeTambahPasangan = !!pasanganId
-  const modeTambahAnak = !!orangtuaId
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <TambahAnggotaClient />
