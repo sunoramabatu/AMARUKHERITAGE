abc900603fb7a8b3f9755d8eaa21434d8a460085
sunoramabatu@gmail.com <emailanda@gmail.com>
2026-03-16 20:53:06 +0700

    perbaiki tambah anak

--- app/tambah/page.js
+++ app/tambah/page.js
@@ -1,9 +1,21 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
 import { Suspense } from "react";
 import TambahAnggotaClient from "./TambahAnggotaClient";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
+
+  const params = useSearchParams()
+
+  const pasanganId = params.get("pasangan")
+  const orangtuaId = params.get("orangtua")
+
+  const modeTambahPasangan = !!pasanganId
+  const modeTambahAnak = !!orangtuaId
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <TambahAnggotaClient />
