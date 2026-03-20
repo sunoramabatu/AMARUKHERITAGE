fe9ecb65729b3e7483e276d16766039d0f92b17c
sunoramabatu@gmail.com <emailanda@gmail.com>
2026-03-20 23:12:42 +0700

    uakueh sing diesdit

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,7 +1,6 @@
 "use client";
 
-export default function SearchBox({ onSearch }){
-    
+export default function SearchBox(){
 
 return (
   <div className="mt-4 flex justify-center">
@@ -9,16 +8,15 @@ return (
       type="text"
       placeholder="🔍 Cari nama anggota..."
       className="w-full max-w-md px-4 py-2 rounded-lg border text-black"
-      onChange={(e)=> onSearch(e.target.value)}
-            onKeyDown={(e)=>{
-            if(e.key==="Enter"){
-                onSearch(e.target.value)
-            }
-            }}
-
+      onChange={(e)=>{
+        window.dispatchEvent(
+          new CustomEvent("searchKeluarga",{
+            detail:e.target.value
+          })
+        )
+      }}
     />
   </div>
 )
-
 
 }
