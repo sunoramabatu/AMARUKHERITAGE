d6b4169865a473fecfdcc6393a34499e35f89027
sunoramabatu@gmail.com <emailanda@gmail.com>
2026-03-20 22:16:25 +0700

    tambah search box

--- app/page.js
+++ app/page.js
@@ -35,6 +35,21 @@ export default async function Home() {
             UNTUK TAMBAH ANGGOTA KELUARGA, MASUKKAN DARI GARIS KETURUNAN AMARI RUKMINI
           </h1>
 
+          <div className="mt-4 flex justify-center">
+            <input
+              type="text"
+              placeholder="🔍 Cari nama anggota..."
+              className="w-full max-w-md px-4 py-2 rounded-lg border text-black"
+              onChange={(e)=>{
+                window.dispatchEvent(
+                  new CustomEvent("searchKeluarga",{
+                    detail:e.target.value
+                  })
+                )
+              }}
+            />
+          </div>
+
           <div className="hidden">
 
             <Link
